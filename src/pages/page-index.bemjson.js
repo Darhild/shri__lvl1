1c183edb39812f47b430a5959387e9baa3c59611
Darhi <isaeva-mariya@yandex.ru>
2019-06-28 23:40:33 +0300

    Сверстана страница index

--- src/pages/page-index.bemjson.js
+++ src/pages/page-index.bemjson.js
@@ -1280,6 +1280,38 @@ module.exports.bemjson = {
               ]
             }
           ]
+        },
+        { block: 'footer',
+          content: [
+            { block: 'footer',
+              elem: 'text',
+              mix: [
+                {block: 'text', mods: {'view': 'primary', 'size': 'l'}}
+              ],
+              content: [
+                { block: 'text',
+                  elem: 'word',
+                  elemMods: {'width': 'l'}
+                },
+                { block: 'text',
+                  elem: 'word',
+                  elemMods: {'width': 's'}
+                }
+              ]
+            },
+            { block: 'footer',
+              elem: 'text',
+              mix: [
+                {block: 'text', mods: {'view': 'primary', 'size': 'l'}}
+              ],
+              content: [
+                { block: 'text',
+                  elem: 'word',
+                  elemMods: {'width': 'l'}
+                }
+              ]
+            }
+          ]
         }
       ]
     }
