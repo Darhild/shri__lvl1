209d5bcfc9d9cfe92eb308154563d4a83361b7cc
Исаева Мария <isaeva-mariya@yandex.ru>
2019-07-19 17:08:36 +0300

    Правки стилей, скрипта

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
 (function() {
+  const layout = document.querySelector(".layout");
+
+  layout.classList.add('theme', 'theme_size_default', 'theme_space_default', 'theme_gap_small', 'theme_color_project-default');
+
   const onoffswitch = document.querySelector(".onoffswitch"),
         themeBlocks = new Set([...document.querySelectorAll(".theme_color_project-default"), ...document.querySelectorAll(".theme_color_project-inverse"), ...document.querySelectorAll(".image")]);
 
