c0f905bed4652ecb3c21393fe1a7da4d8ddfb92a
Исаева Мария <isaeva-mariya@yandex.ru>
2019-07-22 12:31:13 +0300

    Добавлен try/catch

--- build/template-engine.js
+++ build/template-engine.js
@@ -4,58 +4,64 @@
 */
 
 export default function (obj) {
-  let string = "";
-  createDiv (obj);
-  return string;
+  try {
+    let string = "";
+    createDiv (obj);
+    return string;
 
-  function createClassName (obj) {
-    if (obj["block"]) string += `${obj["block"]}`;
+    function createClassName (obj) {
+      if (obj["block"]) string += `${obj["block"]}`;
 
-    if (obj["elem"]) string += `__${obj["elem"]} `;
-    else string += " ";
+      if (obj["elem"]) string += `__${obj["elem"]} `;
+      else string += " ";
 
-    if (obj["mods"]) {
-      for (let prop in obj["mods"]) {
-        string += `${obj["block"]}_${prop}_${obj["mods"][prop]} `
+      if (obj["mods"]) {
+        for (let prop in obj["mods"]) {
+          string += `${obj["block"]}_${prop}_${obj["mods"][prop]} `
+        }
       }
-    }
 
-    if (obj["elemMods"]) {
-      for (let prop in obj["elemMods"]) {
-        string += `${obj["block"]}__${obj["elem"]}_${prop}_${obj["elemMods"][prop]} `
+      if (obj["elemMods"]) {
+        for (let prop in obj["elemMods"]) {
+          string += `${obj["block"]}__${obj["elem"]}_${prop}_${obj["elemMods"][prop]} `
+        }
       }
+
+      if (obj["mix"]) processMix(obj["mix"]);
     }
 
-    if (obj["mix"]) processMix(obj["mix"]);
-  }
-
-  function processMix(obj) {
-    if (Array.isArray(obj)) {
-      obj.forEach(block => {
-        processMix(block);
-      })
+    function processMix(obj) {
+      if (Array.isArray(obj)) {
+        obj.forEach(block => {
+          processMix(block);
+        })
+      }
+      else createClassName(obj);
     }
-    else createClassName(obj);
-  }
 
-  function processContent(obj) {
-    if (Array.isArray(obj)) {
-      obj.forEach(block => {
-        processContent(block);
-      })
+    function processContent(obj) {
+      if (Array.isArray(obj)) {
+        obj.forEach(block => {
+          processContent(block);
+        })
+      }
+      else createDiv(obj);
     }
-    else createDiv(obj);
+
+    function createDiv (obj) {
+      string += `<div class="`;
+
+      createClassName (obj);
+      string = string.trim();
+      string += `">`;
+
+      if (obj["content"]) processContent(obj["content"]);
+
+      string += `</div>`;
+    }
   }
 
-  function createDiv (obj) {
-    string += `<div class="`;
-
-    createClassName (obj);
-    string = string.trim();
-    string += `">`;
-
-    if (obj["content"]) processContent(obj["content"]);
-
-    string += `</div>`;
+  catch (err) {
+    throw new Error(err)  
   }
 }
