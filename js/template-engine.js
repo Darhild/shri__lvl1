99ea5bcbea053505be729352e894ee5e576ac6d9
Darhi <isaeva-mariya@yandex.ru>
2019-07-27 10:34:30 +0300

    Правки стиля

--- js/template-engine.js
+++ js/template-engine.js
@@ -1860,103 +1860,119 @@ const pages = {
                             ]
                           },
                           { block: 'subscription',
-                            elem: 'item',
-                            mix: [
-                             { block: 'text', mods: {'view': 'primary', 'size': 'l'}}
-                            ],
+                            elem: 'advantages',
+                            mix: {block: 'list'},
                             content: [
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 'l'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 'm'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              }
-                            ]
-                          },
-                          { block: 'subscription',
-                            elem: 'item',
-                            mix: [
-                             { block: 'text', mods: {'view': 'primary', 'size': 'l'}}
-                            ],
-                            content: [
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 'l'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              }
-                            ]
-                          },
-                          { block: 'subscription',
-                            elem: 'item',
-                            mix: [
-                             { block: 'text', mods: {'view': 'primary', 'size': 'l'}}
-                            ],
-                            content: [
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 'm'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              }
-                            ]
-                          },
-                          { block: 'subscription',
-                            elem: 'item',
-                            mix: [
-                             { block: 'text', mods: {'view': 'primary', 'size': 'l'}}
-                            ],
-                            content: [
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 'l'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              }
-                            ]
-                          },
-                          { block: 'subscription',
-                            elem: 'item',
-                            mix: [
-                             { block: 'text', mods: {'view': 'primary', 'size': 'l'}}
-                            ],
-                            content: [
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 'm'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              },
-                              { block: 'text',
-                                elem: 'word',
-                                elemMods: {'width': 's'}
-                              }
+                              { block: 'subscription',
+                                elem: 'advantage',
+                                mix: { block: 'list', elem: 'item', elemMods: {'space-b': 's'}},
+                                content:
+                                  { block: 'text',
+                                    mods: {'view': 'primary', 'size': 'l'},
+                                    content: [
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 'l'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 'm'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      }
+                                    ]
+                                  }
+                                },
+                                { block: 'subscription',
+                                elem: 'advantage',
+                                mix: { block: 'list', elem: 'item', elemMods: {'space-b': 's'}},
+                                content:
+                                  { block: 'text',
+                                    mods: {'view': 'primary', 'size': 'l'},
+                                    content: [
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 'l'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      }
+                                    ]
+                                  }
+                                },
+                                { block: 'subscription',
+                                elem: 'advantage',
+                                mix: { block: 'list', elem: 'item', elemMods: {'space-b': 's'}},
+                                content:
+                                  { block: 'text',
+                                    mods: {'view': 'primary', 'size': 'l'},
+                                    content: [
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 'm'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      }
+                                    ]
+                                  }
+                                },
+                                { block: 'subscription',
+                                elem: 'advantage',
+                                mix: { block: 'list', elem: 'item', elemMods: {'space-b': 's'}},
+                                content:
+                                  { block: 'text',
+                                    mods: {'view': 'primary', 'size': 'l'},
+                                    content: [
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 'l'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      }
+                                    ]
+                                  }
+                                },
+                                { block: 'subscription',
+                                elem: 'advantage',
+                                mix: { block: 'list', elem: 'item', elemMods: {'space-b': 's'}},
+                                content:
+                                  { block: 'text',
+                                    mods: {'view': 'primary', 'size': 'l'},
+                                    content: [
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 'm'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      },
+                                      { block: 'text',
+                                        elem: 'word',
+                                        elemMods: {'width': 's'}
+                                      }
+                                    ]
+                                  }
+                                }
                             ]
                           }
                         ]
